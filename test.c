4108fe7dea01639852ab8f0b0258a736e07fc504
caaal0 <jcsagun@up.edu.ph>
2024-05-20 19:18:28 +0800

    test file

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+int main(){
+
+    
+    return 0;
+}
